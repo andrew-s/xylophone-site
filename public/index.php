067b028264d139d5f40016cca0cc38ea25ac4640
Andrew Seymour <commit@andrewseymour.co.uk>
2013-12-17 16:48:49 +0000

    Added bootflat and first test page (will use xylophone eventually)

---
+++ public/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Xylophone PHP Framework</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap -->
+  <link href="/assets/bootflat/bootstrap/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="/assets/bootflat/css/font-awesome.min.css" rel="stylesheet" media="screen">
+  <link href="/assets/bootflat/css/frame-template.css" rel="stylesheet" type='text/css'>
+  <link href="/assets/bootflat/css/default.css" rel="stylesheet" type='text/css'>
+  <link href="/assets/bootflat/css/bootflat.css" rel="stylesheet" media="screen">
+  <link href="/assets/bootflat/css/bootflat-extensions.css" rel="stylesheet" media="screen">
+  <link href="/assets/bootflat/css/bootflat-square.css" rel="stylesheet" media="screen">
+
+</head>
+<body>
+
+  <header class="navbar navbar-inverse navbar-fixed-top bf-docs-nav" role="banner">
+    <div class="container">
+      <div class="navbar-header">
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bf-navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="../index.html" class="navbar-brand">Xylophone PHP Framework</a>
+      </div>
+      <nav class="collapse navbar-collapse bf-navbar-collapse" role="navigation">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="../getting-started.html">Getting Started</a></li></li>
+          <li class="active"><a href="../docs/components.html">Components</a></li>
+          <li><a href="/user_guide/">User Guide</a></li>
+
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <div class="bf-header" id="content">
+    <div class="container">
+      <div class="pull-right" style="margin-top: 26px;">
+        <!-- adsense -->
+      </div>
+      <h1>Why Xylophone?</h1>
+      <p>
+        Developing applications has never been easier with the wide range of frameworks available, <a href="http://www.codeigniter.com/">CodeIgniter</a> was one of the frameworks
+        at the beginning - we've forked it and added some features like HMVC while still providing you with an upgrade-path for your exisitng applications.
+      </p>
+    </div>
+  </div>
+
+  <footer class="bf-footer bf-footer-margin" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 style="margin-bottom: 15px;">Connect With Us</h3>
+          <div class="bf-connect">
+            <p class="social">
+              <a title="Twitter" href="https://twitter.com/" target="_blank" rel="external nofollow"><i class="fa fa-twitter"></i></a>
+              <a title="Facebook" href="https://www.facebook.com/" target="_blank" rel="external nofollow"><i class="fa fa-facebook"></i></a>
+              <a title="Google+" href="https://plus.google.com/" target="_blank" rel="external nofollow"><i class="fa fa-google-plus"></i></a>
+              <a title="Github" href="https://github.com/" target="_blank" rel="external nofollow"><i class="fa fa-github"></i></a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+      <ul class="bf-masthead-links">
+        <li>xylophone.io</li>
+        <li>extra footer</li>
+      </ul>
+    </div>
+  </footer>
+
+  <script src="/assets/bootflat/js/bootstrap.min.js"></script>
+</body>
+</html>
